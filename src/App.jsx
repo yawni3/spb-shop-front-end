ab00a6440367f1430e8552cdb7d0916d1b8faf0b
yawnie <eyceyheycey@gmail.com>
2026-06-27 14:51:01 +0300

    some changes again

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home.jsx";
+
 import Shop from "./pages/Shop/Shop.jsx";
 import About from "./pages/About/About.jsx";
 import ProductDetails from "./pages/Product/Productdetail.jsx";
 import Layout from "./Baselayout.jsx";
 import Checkout from './pages/Checkout/Checkout.jsx';
 import Cart from "./pages/Cart/Cart.jsx";
-
+import Home from "./pages/Home/Home.jsx"
 
 function App() {
   return (
