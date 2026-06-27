199669a33cd7fbe62cd7612491d671c4eae221df
yawnie <eyceyheycey@gmail.com>
2026-06-27 15:49:35 +0300

    Change import

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import ConfirmDialog from "../../components/Confirm-dialog.jsx";
+import ConfirmDialog from "../../components/ConfirmDialog.jsx";
 import "./cart.css";
 
 // ⭐ Icon import'ları
