690a7680093ed88d1b5d9986bd806d163446c216
yawnie <eyceyheycey@gmail.com>
2026-06-27 15:51:49 +0300

    add confirm dialog

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import ConfirmDialog from "../../components/ConfirmDialog.jsx";
+import ConfirmDialog from "../../components/ConfirmDialog";
 import "./cart.css";
 
 // ⭐ Icon import'ları
