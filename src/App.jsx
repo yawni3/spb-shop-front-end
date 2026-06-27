3c503ece506449e4a9831ae45ecaa2706822a8fe
yawnie <eyceyheycey@gmail.com>
2026-06-27 15:26:14 +0300

    changed file import

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 
-import Shop from "./pages/shop/shop.jsx";
-import About from "./pages/about/about.jsx";
-import ProductDetails from "./pages/product/productdetail.jsx";
+// ⭐ GitHub'daki gerçek dosya isimlerine göre düzenlendi
+import Shop from "./pages/Shop/shop.jsx";
+import About from "./pages/About/About.jsx";
+import ProductDetails from "./pages/Product/ProductDetail.jsx";
 import Layout from "./Baselayout.jsx";
-import Checkout from './pages/checkout/checkout.jsx';
-import Cart from "./pages/cart/cart.jsx";
-import Home from "./pages/home/home.jsx"
-
+import Checkout from './pages/checkout/checkOut.jsx';  // ⭐ DİKKAT: checkOut.jsx
+import Cart from "./pages/Cart/Cart.jsx";
+import Home from "./pages/Home/home.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        
-         <Route index element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
-        
-        {/* ⭐ Shop ve filtreli sayfalar */}
         <Route path="/shop" element={<Shop />} />
         <Route path="/assets" element={<Shop />} />
         <Route path="/apps" element={<Shop />} />
         <Route path="/wallpapers" element={<Shop />} />
         <Route path="/freebies" element={<Shop />} />
-        
-        {/* ⭐ Ürün detay */}
         <Route path="/product/:slug" element={<ProductDetails />} />
-        
-        {/* ⭐ Sepet ve Ödeme */}
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
       </Route>
